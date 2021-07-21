baa767fdd7eb69a9a1041036946d66c32bf0d717
= <=>
2021-07-21 08:29:52 +0700

    Bai 15 bai tap 5

--- bai5/bai5.js
+++ bai5/bai5.js
@@ -6,10 +6,10 @@ function drawtable(){
         data = "<table border='1' cellpadding='10px' style='border-collapse: collapse;'> <tr><th>ID</th><th class='name'>Tên</th><th class='price'>Giá</th><th class='quantity'>Số Lượng</th></tr>"
     for (i=0;i<SP.length;i++) {
         data += `<tr><td class="id">${(i+1)}</td>
-                     <td class="name">${SP[i][0]}</td>
-                     <td class="price">${SP[i][1]}</td>
-                     <td class="quantity">${SP[i][2]}</td>
-                     <td><button onclick="replace('${i}')">Sửa</button></td>
+                     <td class="name" id="${i}0">${SP[i][0]}</td>
+                     <td class="price" id="${i}1">${SP[i][1]}</td>
+                     <td class="quantity" id="${i}2">${SP[i][2]}</td>
+                     <td><button onclick="replace(${i})">Sửa</button></td>
                      <td><button onclick="dele('${i}')">Xoá</button></td>
                      </tr>`
     }
@@ -37,12 +37,22 @@ function add() {
     drawtable()
 }
 function replace(value) {
-    let tensua = prompt("Nhập tên sản phầm sửa:");
-    let giasua = prompt("Nhập giá sản phầm sửa:");
-    let soluongsua = prompt("Nhập số lượng sản phầm sửa:");
-    SP[value][0] = tensua;
-    SP[value][1] = giasua;
-    SP[value][2] = soluongsua;
+    document.getElementById(value+'0').innerHTML = `<input type="text" id="change1" onchange="saveName(${value})" >`;
+    document.getElementById(value+'1').innerHTML = `<input type="text" id="change2" onchange="savePrice(${value})">`;
+    document.getElementById(value+'2').innerHTML = `<input type="text" id="change3" onchange="saveQuantity(${value})">`;
+
+}
+function saveName(value) {
+    let tensua = document.getElementById("change1").value;
+    SP[value][0] = tensua
+}
+function savePrice(value) {
+    let giasua = document.getElementById("change2").value;
+    SP[value][1] = giasua
+}
+function saveQuantity(value) {
+    let soluongsua = document.getElementById("change3").value;
+    SP[value][2] = soluongsua
     drawtable()
 }
 function  dele(value) {
